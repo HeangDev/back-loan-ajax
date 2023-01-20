77b5403b1ca78a01a83284de0951d359d4ad8ff4
Kimheang-Developer <simkimheang4@gmail.com>
2023-01-20 15:19:51 +0700

    Heang Updte

--- resources/views/report/withdraw.blade.php
+++ resources/views/report/withdraw.blade.php
@@ -85,6 +85,7 @@
                 processing: true,
                 serverSide: true,
                 lengthChange: false,
+                dom: 'Bfrtip',
                 buttons: [ 'copy', 'csv', 'excel', 'pdf', 'print' ],
                 "language": {
                     "lengthMenu": "แสดง _MENU_ แถวต่อหน้า",
